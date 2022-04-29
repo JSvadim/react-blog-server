19d4e6f54d05be78c07557b9bb485d6952784dee
JSvadim <sadomazo323@gmail.com>
2022-04-29 20:58:19 +0300

    Added user id to user dto.

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -2,9 +2,11 @@ export class UserDto {
     nickname;
     email;
     gender;
+    id;
     constructor(user) {
         this.nickname = user.nickname;
         this.email = user.email;
         this.gender = user.gender;
+        this.id = user.id;
     }
 }
