928410ac272dad5c54bfabe926cf25c0ec00aef2
JSvadim <sadomazo323@gmail.com>
2022-09-02 20:38:07 +0300

    Added two error types
    First - when somebody tries to change html markup and
    breaks form validation;
    Second - when file size is too big;

--- exceptions/api-error.js
+++ exceptions/api-error.js
@@ -12,4 +12,26 @@ export class ApiError extends Error {
     static badRequest(message, errors) {
         return new ApiError(400, message, errors);
     }
+
+    static htmlChange() {
+        return new ApiError(500, "Don't change html markup, sir.");
+    }
+
+    static FileSizeIsTooBig(filename) {
+        if(Array.isArray(filename)) {
+            if(filename.length > 1) {
+                const fileNames = filename.join(", ");
+                const message = `Your files: ${fileNames} ARE TOO BIG`
+                return new ApiError(413, message);
+            }
+            const message = `Your file: ${filename[0]} IS TOO BIG`
+            return new ApiError(413, message);
+        }
+        const message = `Your file: ${filename} IS TOO BIG`
+        return new ApiError(413, message);
+    }
+
+    static unknown() {
+        return new ApiError(500, "Unknown server error");
+    }
 }
