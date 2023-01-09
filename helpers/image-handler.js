67b790e8c2e70f1a78343d84884c6a9bec8d760b
JSvadim <sadomazo323@gmail.com>
2023-01-09 19:21:04 +0300

    New approach to store images
    I found an issue. If I made changes into server-side and updated
    my deployed server, all pictures would dissapear. It would happen
    because folder with pictures is in .gitignore, and a platform
    gets new version of server from github without any "pictures" folder.
    
    Founded solution is "https://imagekit.io" service, which allows
    to store images and applies some optimizations, which means I
    don't need to compress image by myself anymore.
    
    Also the service allows to add parameters into url while requesting
    image from client side and get as optimized image as it should be.

--- helpers/image-handler.js
+++ helpers/image-handler.js
@@ -1,13 +1,10 @@
 // third party 
-import * as path from "path";
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import { v1 } from "uuid";
-import fs from "fs/promises";
 import imageMin from "imagemin";
 import imageSize from "image-size";
 import imageMozjpeg from "imagemin-mozjpeg";
 import imageminPngquant from "imagemin-pngquant";
+import { imageKit } from "./image-kit.js";
 
 
 export class ImageHandler {
@@ -50,25 +47,21 @@ export class ImageHandler {
         return compressedImage
     }
     static async saveImages(files) {
-        // saves original image and compressed image, 
+        // saves image to imagekit.io, 
         // returns string containing images names separated with " " 
         const imagesNamesArray = []
         await Promise.all(
             files.map(async (file) => {
                 const imageSizes = imageSize(file.buffer);
-                const __dirname = dirname(fileURLToPath(import.meta.url));
                 const imageName = v1() + '.' + imageSizes.type;
-                imagesNamesArray.push(imageName);
 
                 // saving file
-                const pathToImage = path.join(__dirname, '..', 'public', 'images', imageName);
-                await fs.writeFile(pathToImage, file.buffer, 'binary');
-                
-                // compressing file and saving 
-                if(imageSizes.type === "gif") return // don't compress gif
-                const pathToCompressedImage = path.join(__dirname, '..', 'public', 'images-compressed', imageName);
-                const compressedImage = await this.compressImage(pathToImage);
-                await fs.writeFile(pathToCompressedImage, compressedImage[0].data, 'binary');
+                const uploadedImage = await imageKit.upload({
+                    file: file.buffer,
+                    fileName: imageName,
+                    folder: "blog-images"
+                })
+                imagesNamesArray.push(uploadedImage.name);
             })
         );
         return imagesNamesArray.join(" "); 
---
+++ helpers/image-kit.js
@@ -0,0 +1,8 @@
+// third-party
+import ImageKit from "imagekit";
+
+export const imageKit = new ImageKit({
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint: process.env.IMAGEKIT_URL
+}) 
